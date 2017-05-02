7313abdd7c52ac4e5b8b7e884d7907e259393195
bfr3688 <jackshamu@yahoo.com>
2017-05-02 13:46:22 -0700

    Changed to take multiple data streams

---
+++ www/Archive/longpolling_mod-multiple/datasrv1.php
@@ -0,0 +1,20 @@
+<?php 
+$filename = "/sys/bus/w1/devices/28-0000084a343c/w1_slave";
+$file = fopen($filename,"r");
+
+if ( $file == false ){
+echo ( "Error in opening file" );
+}
+
+$reading = fread( $file, filesize( $filename));
+fclose( $file );
+
+preg_match("/t=(.+)/", preg_split("/\n/", $reading)[1], $matches);
+$degreesc = $matches[1] / 1000;
+$degreesf = round($degreesc*9/5+32, "1");
+
+echo "Current Temperature: ";
+echo $degreesf;
+echo " F";
+
+?>
